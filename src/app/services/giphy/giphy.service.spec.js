a0e7d9ea6872a25a1242411343dd4cdc8098f140
Phong Huynh <xphong@gmail.com>
2016-11-19 15:49:53 -0500

    Use actual giphy data for service test

--- src/app/services/giphy/giphy.service.spec.js
+++ src/app/services/giphy/giphy.service.spec.js
@@ -6,7 +6,32 @@ describe('Service: GiphyService', function () {
       $httpBackend,
       giphyServiceData;
 
-  giphyServiceData = [{'some': 'test data'}];
+  giphyServiceData = [{
+    'type': 'gif',
+    'id': '3o6Ztb8DOw7q6VNYw8',
+    'slug': 'sharon-jones-3o6Ztb8DOw7q6VNYw8',
+    'url': 'http:\/\/giphy.com\/gifs\/sharon-jones-3o6Ztb8DOw7q6VNYw8',
+    'bitly_gif_url': 'http:\/\/gph.is\/2g5dJ5e',
+    'bitly_url': 'http:\/\/gph.is\/2g5dJ5e',
+    'embed_url': 'http:\/\/giphy.com\/embed\/3o6Ztb8DOw7q6VNYw8',
+    'username': '',
+    'source': '',
+    'rating': 'g',
+    'content_url': '',
+    'source_tld': '',
+    'source_post_url': '',
+    'is_indexable': 0,
+    'import_datetime': '2016-11-19 16:34:04',
+    'trending_datetime': '2016-11-19 16:39:32',
+    'images': {
+      'downsized': {
+        'url': 'http:\/\/media0.giphy.com\/media\/3o6Ztb8DOw7q6VNYw8\/giphy-tumblr.gif',
+        'width': '250',
+        'height': '186',
+        'size': '1325251'
+      }
+     }
+   }];
 
   beforeEach(inject(function(_$httpBackend_, _GiphyService_) {
       $httpBackend = _$httpBackend_;
