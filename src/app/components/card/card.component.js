42d1683f7c47963c04d9f982d28c7070e169a9de
Phong Huynh <xphong@gmail.com>
2016-11-19 20:20:47 -0500

    Move code to card component, create component unit test

--- src/app/components/card/card.component.js
+++ src/app/components/card/card.component.js
@@ -5,7 +5,7 @@
       .module('giphyGalleryApp')
       .component('ggCard', {
         bindings: {
-          
+          data: '='
         },
         templateUrl: 'components/card/card.tpl.html',
         controller: CardComponentCtrl,
@@ -15,7 +15,31 @@
     CardComponentCtrl.$inject = [];
 
     function CardComponentCtrl () {
-        var vm = this;
+      var vm = this;
+
+      vm.likeGiphy = likeGiphy;
+      vm.formatDateString = formatDateString;
+
+      function likeGiphy () {
+        if (!vm.data.likeCount) {
+          vm.data.likeCount = 0;
+          vm.data.likeCount += 1;
+        } else {
+          vm.data.likeCount += 1;
+        }
+      }
+
+      function formatDateString(dateString) {
+        return formatDate(new Date(dateString));
+      }
+
+      function formatDate (date) {
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        var year = date.getFullYear();
+
+        return year + "/" + month + "/" + day;
+      }  
     }
 
 })(angular);
